d92867fb46653c32c43b9ad3344c9c148950014f
Colin White <clwhite814@gmail.com>
2019-04-02 21:25:09 -0500

    wip

---
+++ RomanNumerals.Test/converting_numbers_into_roman_numerals.cs
@@ -0,0 +1,26 @@
+using System;
+using Xunit;
+
+namespace RomanNumerals.Test
+{
+    public class converting_numbers_into_roman_numerals
+    {
+        [Fact]
+        public void one_should_translate_to_I()
+        {
+            // Arrange 
+            // Act 
+            // Assert
+
+        }
+
+        [Fact]
+        public void five_should_translate_to_V()
+        {
+            // Arrange 
+            // Act 
+            // Assert
+
+        }
+    }
+}
