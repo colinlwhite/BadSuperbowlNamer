e1986b7f8bf9d640fd870ae7f02a4b5eccf28b98
Colin White <clwhite814@gmail.com>
2019-04-03 20:44:43 -0500

    wip

--- BadSuperbowlNamer/TranslateEngine.cs
+++ BadSuperbowlNamer/TranslateEngine.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace BadSuperbowlNamer
 {
     public class TranslateEngine
     {
         // public method that does math
+        public void DoWork()
+        {
+            Console.WriteLine("Does this fix it?");
+        }
+
     }
 }
--- RomanNumerals.Test/converting_numbers_into_roman_numerals.cs
+++ RomanNumerals.Test/converting_numbers_into_roman_numerals.cs
@@ -1,14 +1,17 @@
 using System;
 using Xunit;
 
-namespace RomanNumerals.Test
+//namespace RomanNumerals.Test
+namespace BadSuperbowlNamer.Test
 {
     public class converting_numbers_into_roman_numerals
     {
         [Fact]
         public void one_should_translate_to_I()
         {
-            // Arrange 
+            // Arrange - giving context - given
+            var converter = new TranslateEngine();
+
             // Act 
             // Assert
 
